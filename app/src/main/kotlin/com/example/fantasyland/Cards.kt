90c1f08b291382061848b2c56c9cfe73fb6e7eec
figlo <miro@figlar.sk>
2022-01-14 11:06:15 +0100

    removed unused classes and functions

--- app/src/main/kotlin/com/example/fantasyland/Cards.kt
+++ app/src/main/kotlin/com/example/fantasyland/Cards.kt
@@ -1,37 +1,9 @@
 package com.example.fantasyland
 
-import androidx.core.text.HtmlCompat
-
 data class Card(val face: CardFace, val suit: CardSuit, val file: String) {
-    override fun toString() = "${face.abbr}${suit.abbr}"
-
-    val htmlColored = "<font color=${suit.hexColor}>$this</font>"
 }
 
 open class Cards(val cards: MutableList<Card>) {
-    private var sortSwitch = true
-
-    fun sort() {
-        if (sortSwitch) sortByColorAndRank() else sortByRankAndColor()
-        sortSwitch = !sortSwitch
-    }
-
-    private fun sortByRankAndColor() =
-        cards.sortWith(
-            compareBy(
-                { -it.face.rankAceHigh },
-                { it.suit }
-            )
-        )
-
-    private fun sortByColorAndRank() =
-        cards.sortWith(
-            compareBy(
-                { it.suit },
-                { -it.face.rankAceHigh }
-            )
-        )
-
     protected fun sortByCountAndRank() =
         cards.sortWith(
             compareBy(
@@ -39,38 +11,4 @@ open class Cards(val cards: MutableList<Card>) {
                 { -it.face.rankAceHigh }
             )
         )
-
-    fun display() =
-        HtmlCompat.fromHtml(
-            cards.joinToString(separator = " ") { it.htmlColored },
-            HtmlCompat.FROM_HTML_MODE_LEGACY
-        )
-
-    override fun toString() = cards.joinToString(separator = " ", prefix =  "[", postfix = "]")
-}
-
-class Deck(cards: MutableList<Card> = mutableListOf()) : Cards(cards) {
-    fun loadFull() {
-        cards.clear()
-        var file: String
-        CardFace.values().forEach { cardFace ->
-            CardSuit.values().forEach { cardSuit ->
-                file = "card_${(cardFace.abbr).lowercase()}${cardSuit.abbr}.png"
-//                Log.d(file)
-                cards.add(Card(cardFace, cardSuit, file))
-            }
-        }
-    }
-
-    fun drawCards(quantity: Int): Cards {
-        require(quantity > 0 && quantity <= cards.size) { "quantity (must be > 0 and <= size of deck.cards): $quantity" }
-        val drawnCards: MutableList<Card> = mutableListOf()
-        (1..quantity).forEach { _ ->
-            cards.random().let {
-                drawnCards.add(it)
-                cards.remove(it)
-            }
-        }
-        return Cards(drawnCards)
-    }
 }
