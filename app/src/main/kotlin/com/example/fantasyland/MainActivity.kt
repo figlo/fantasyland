05f579edf933b8aa46c46c443f54ce7bc289e073
figlo <miro@figlar.sk>
2022-02-18 11:38:47 +0100

    refactoring

--- app/src/main/kotlin/com/example/fantasyland/MainActivity.kt
+++ app/src/main/kotlin/com/example/fantasyland/MainActivity.kt
@@ -12,15 +12,19 @@ import com.example.fantasyland.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+    }
+    private val navController by lazy {
+        navHostFragment.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(navController.graph)
-
         setSupportActionBar(binding.toolbar)
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
@@ -30,10 +34,7 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        return NavigationUI.onNavDestinationSelected(item, navController)
+    override fun onOptionsItemSelected(item: MenuItem) =
+        NavigationUI.onNavDestinationSelected(item, navController)
                 || super.onOptionsItemSelected(item)
-    }
 }
