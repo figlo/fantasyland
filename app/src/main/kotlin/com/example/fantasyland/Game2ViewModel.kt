66480d3e1ff1411a2ea79bf07de2b7bb23c9d903
figlo <miro@figlar.sk>
2022-04-11 23:11:50 +0200

    dealt cards

--- app/src/main/kotlin/com/example/fantasyland/Game2ViewModel.kt
+++ app/src/main/kotlin/com/example/fantasyland/Game2ViewModel.kt
@@ -3,10 +3,17 @@ package com.example.fantasyland
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class Game2ViewModel: ViewModel() {
+class Game2ViewModel : ViewModel() {
     var cards = MutableLiveData<MutableList<Card?>>()
 
     init {
-        cards.value = MutableList(30) { null }
+        val _cards: MutableList<Card?> = MutableList(30) { null }
+
+        for (i in _cards.indices) {
+            if ( i in 14..27)
+            _cards[i - 1] = dealCard()
+        }
+
+        cards.value = _cards
     }
 }
