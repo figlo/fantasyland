027633b3b8e686450986743a954732e2e7327d57
figlo <miro@figlar.sk>
2022-04-12 10:25:47 +0200

    number of dealt cards

--- app/src/main/kotlin/com/example/fantasyland/Game2ViewModel.kt
+++ app/src/main/kotlin/com/example/fantasyland/Game2ViewModel.kt
@@ -1,17 +1,22 @@
 package com.example.fantasyland
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.preference.PreferenceManager
 
-class Game2ViewModel : ViewModel() {
+class Game2ViewModel(application: Application) : AndroidViewModel(application) {
     var cards = MutableLiveData<MutableList<Card?>>()
 
     init {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(application)
+        val numberOfCardsInFantasyLand: Int = preferences.getString("number_of_cards_in_fantasy_land", "14")?.toInt()!!
+
         val _cards: MutableList<Card?> = MutableList(30) { null }
 
         for (i in _cards.indices) {
-            if ( i in 14..27)
-            _cards[i - 1] = dealCard()
+            if ( i in 13..(12 + numberOfCardsInFantasyLand))
+            _cards[i] = dealCard()
         }
 
         cards.value = _cards
