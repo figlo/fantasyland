74c1dc6ef9a7130d9ead1537cc6b7f1d6d5e1e46
figlo <miro@figlar.sk>
2022-05-07 22:02:00 +0200

    refactoring

--- app/src/main/kotlin/com/example/fantasyland/GameFragment.kt
+++ app/src/main/kotlin/com/example/fantasyland/GameFragment.kt
@@ -31,120 +31,6 @@ class GameFragment : Fragment() {
         Timber.i("onCreateView called")
 
         binding = FragmentGameBinding.inflate(inflater)
-
-        fun cardImageResource(card: Card?): Int = resources.getIdentifier(fileName(card), "drawable", requireContext().packageName)
-
-        viewModel.isMovingPhaseDone.observe(viewLifecycleOwner) { newIsMovingPhaseDone ->
-            if (newIsMovingPhaseDone) {
-                binding.buttonSort.visibility = View.GONE
-                binding.buttonSetAllCards.visibility = View.GONE
-                binding.buttonDone.visibility = View.VISIBLE
-            } else {
-                binding.buttonSort.visibility = View.VISIBLE
-                binding.buttonSetAllCards.visibility = View.VISIBLE
-                binding.buttonDone.visibility = View.GONE
-            }
-        }
-
-        viewModel.cards.observe(viewLifecycleOwner) { newCards ->
-            var i = 0
-            binding.cardView1.tag = i++
-            binding.cardView2.tag = i++
-            binding.cardView3.tag = i++
-            binding.cardView4.tag = i++
-            binding.cardView5.tag = i++
-            binding.cardView6.tag = i++
-            binding.cardView7.tag = i++
-            binding.cardView8.tag = i++
-            binding.cardView9.tag = i++
-            binding.cardView10.tag = i++
-            binding.cardView11.tag = i++
-            binding.cardView12.tag = i++
-            binding.cardView13.tag = i++
-            binding.cardView14.tag = i++
-            binding.cardView15.tag = i++
-            binding.cardView16.tag = i++
-            binding.cardView17.tag = i++
-            binding.cardView18.tag = i++
-            binding.cardView19.tag = i++
-            binding.cardView20.tag = i++
-            binding.cardView21.tag = i++
-            binding.cardView22.tag = i++
-            binding.cardView23.tag = i++
-            binding.cardView24.tag = i++
-            binding.cardView25.tag = i++
-            binding.cardView26.tag = i++
-            binding.cardView27.tag = i++
-            binding.cardView28.tag = i++
-            binding.cardView29.tag = i++
-            binding.cardView30.tag = i
-
-            binding.cardView1.setImageResource(cardImageResource(newCards[0]))
-            binding.cardView2.setImageResource(cardImageResource(newCards[1]))
-            binding.cardView3.setImageResource(cardImageResource(newCards[2]))
-            binding.cardView4.setImageResource(cardImageResource(newCards[3]))
-            binding.cardView5.setImageResource(cardImageResource(newCards[4]))
-            binding.cardView6.setImageResource(cardImageResource(newCards[5]))
-            binding.cardView7.setImageResource(cardImageResource(newCards[6]))
-            binding.cardView8.setImageResource(cardImageResource(newCards[7]))
-            binding.cardView9.setImageResource(cardImageResource(newCards[8]))
-            binding.cardView10.setImageResource(cardImageResource(newCards[9]))
-            binding.cardView11.setImageResource(cardImageResource(newCards[10]))
-            binding.cardView12.setImageResource(cardImageResource(newCards[11]))
-            binding.cardView13.setImageResource(cardImageResource(newCards[12]))
-            binding.cardView14.setImageResource(cardImageResource(newCards[13]))
-            binding.cardView15.setImageResource(cardImageResource(newCards[14]))
-            binding.cardView16.setImageResource(cardImageResource(newCards[15]))
-            binding.cardView17.setImageResource(cardImageResource(newCards[16]))
-            binding.cardView18.setImageResource(cardImageResource(newCards[17]))
-            binding.cardView19.setImageResource(cardImageResource(newCards[18]))
-            binding.cardView20.setImageResource(cardImageResource(newCards[19]))
-            binding.cardView21.setImageResource(cardImageResource(newCards[20]))
-            binding.cardView22.setImageResource(cardImageResource(newCards[21]))
-            binding.cardView23.setImageResource(cardImageResource(newCards[22]))
-            binding.cardView24.setImageResource(cardImageResource(newCards[23]))
-            binding.cardView25.setImageResource(cardImageResource(newCards[24]))
-            binding.cardView26.setImageResource(cardImageResource(newCards[25]))
-            binding.cardView27.setImageResource(cardImageResource(newCards[26]))
-            binding.cardView28.setImageResource(cardImageResource(newCards[27]))
-            binding.cardView29.setImageResource(cardImageResource(newCards[28]))
-            binding.cardView30.setImageResource(cardImageResource(newCards[29]))
-        }
-
-        viewModel.isGameFinished.observe(viewLifecycleOwner) { newIsGameFinished ->
-            if (newIsGameFinished) {
-                binding.apply {
-                    buttonSort.visibility = View.GONE
-                    buttonSetAllCards.visibility = View.GONE
-                    buttonDone.visibility = View.GONE
-                    buttonNewGame.visibility = View.VISIBLE
-                    buttonShare.visibility = View.VISIBLE
-                    if (viewModel.isValidResult) {
-                        val resultOKColor: Int = ContextCompat.getColor(requireContext(), R.color.resultOK)
-                        topRowResult.visibility = View.VISIBLE
-                        middleRowResult.visibility = View.VISIBLE
-                        bottomRowResult.visibility = View.VISIBLE
-                        bottomRowResult.text = viewModel.bottomRowResult.toString()
-                        middleRowResult.text = viewModel.middleRowResult.toString()
-                        topRowResult.text = viewModel.topRowResult.toString()
-                        finalResult.text = viewModel.finalResult.toString()
-                        finalResult.setTextColor(resultOKColor)
-                    } else {
-                        val resultXColor: Int = ContextCompat.getColor(requireContext(), R.color.resultX)
-                        finalResult.text = resources.getString(R.string.result_x)
-                        finalResult.setTextColor(resultXColor)
-                    }
-                    finalResult.visibility = View.VISIBLE
-                }
-
-                if (viewModel.isRepeatedFantasy) {
-                    binding.apply {
-                        newFantasyLand.text = resources.getString(R.string.new_fantasyland)
-                    }
-                }
-            }
-        }
-
         return binding.root
     }
 
@@ -218,6 +104,8 @@ class GameFragment : Fragment() {
         dealtCardViews.drop(numberOfCardsInFantasyLand).forEach { it.visibility = View.GONE }
 
         // cardView functions
+        setCardViewsTags()
+
         var selectedView: ImageView? = null
 
         fun select(cardView: ImageView) {
@@ -303,6 +191,121 @@ class GameFragment : Fragment() {
                 .putExtra(Intent.EXTRA_TEXT, binding.finalResult.text)
             startActivity(shareIntent)
         }
+
+        viewModel.isMovingPhaseDone.observe(viewLifecycleOwner) { newIsMovingPhaseDone ->
+            if (newIsMovingPhaseDone) {
+                binding.buttonSort.visibility = View.GONE
+                binding.buttonSetAllCards.visibility = View.GONE
+                binding.buttonDone.visibility = View.VISIBLE
+            } else {
+                binding.buttonSort.visibility = View.VISIBLE
+                binding.buttonSetAllCards.visibility = View.VISIBLE
+                binding.buttonDone.visibility = View.GONE
+            }
+        }
+
+        viewModel.isGameFinished.observe(viewLifecycleOwner) { newIsGameFinished ->
+            if (newIsGameFinished) {
+                binding.apply {
+                    buttonSort.visibility = View.GONE
+                    buttonSetAllCards.visibility = View.GONE
+                    buttonDone.visibility = View.GONE
+                    buttonNewGame.visibility = View.VISIBLE
+                    buttonShare.visibility = View.VISIBLE
+                    if (viewModel.isValidResult) {
+                        val resultOKColor: Int = ContextCompat.getColor(requireContext(), R.color.resultOK)
+                        topRowResult.visibility = View.VISIBLE
+                        middleRowResult.visibility = View.VISIBLE
+                        bottomRowResult.visibility = View.VISIBLE
+                        bottomRowResult.text = viewModel.bottomRowResult.toString()
+                        middleRowResult.text = viewModel.middleRowResult.toString()
+                        topRowResult.text = viewModel.topRowResult.toString()
+                        finalResult.text = viewModel.finalResult.toString()
+                        finalResult.setTextColor(resultOKColor)
+                    } else {
+                        val resultXColor: Int = ContextCompat.getColor(requireContext(), R.color.resultX)
+                        finalResult.text = resources.getString(R.string.result_x)
+                        finalResult.setTextColor(resultXColor)
+                    }
+                    finalResult.visibility = View.VISIBLE
+                }
+
+                if (viewModel.isRepeatedFantasy) {
+                    binding.apply {
+                        newFantasyLand.text = resources.getString(R.string.new_fantasyland)
+                    }
+                }
+            }
+        }
+
+        viewModel.cards.observe(viewLifecycleOwner) { newCards ->
+            binding.cardView1.setImageResource(cardImageResource(newCards[0]))
+            binding.cardView2.setImageResource(cardImageResource(newCards[1]))
+            binding.cardView3.setImageResource(cardImageResource(newCards[2]))
+            binding.cardView4.setImageResource(cardImageResource(newCards[3]))
+            binding.cardView5.setImageResource(cardImageResource(newCards[4]))
+            binding.cardView6.setImageResource(cardImageResource(newCards[5]))
+            binding.cardView7.setImageResource(cardImageResource(newCards[6]))
+            binding.cardView8.setImageResource(cardImageResource(newCards[7]))
+            binding.cardView9.setImageResource(cardImageResource(newCards[8]))
+            binding.cardView10.setImageResource(cardImageResource(newCards[9]))
+            binding.cardView11.setImageResource(cardImageResource(newCards[10]))
+            binding.cardView12.setImageResource(cardImageResource(newCards[11]))
+            binding.cardView13.setImageResource(cardImageResource(newCards[12]))
+            binding.cardView14.setImageResource(cardImageResource(newCards[13]))
+            binding.cardView15.setImageResource(cardImageResource(newCards[14]))
+            binding.cardView16.setImageResource(cardImageResource(newCards[15]))
+            binding.cardView17.setImageResource(cardImageResource(newCards[16]))
+            binding.cardView18.setImageResource(cardImageResource(newCards[17]))
+            binding.cardView19.setImageResource(cardImageResource(newCards[18]))
+            binding.cardView20.setImageResource(cardImageResource(newCards[19]))
+            binding.cardView21.setImageResource(cardImageResource(newCards[20]))
+            binding.cardView22.setImageResource(cardImageResource(newCards[21]))
+            binding.cardView23.setImageResource(cardImageResource(newCards[22]))
+            binding.cardView24.setImageResource(cardImageResource(newCards[23]))
+            binding.cardView25.setImageResource(cardImageResource(newCards[24]))
+            binding.cardView26.setImageResource(cardImageResource(newCards[25]))
+            binding.cardView27.setImageResource(cardImageResource(newCards[26]))
+            binding.cardView28.setImageResource(cardImageResource(newCards[27]))
+            binding.cardView29.setImageResource(cardImageResource(newCards[28]))
+            binding.cardView30.setImageResource(cardImageResource(newCards[29]))
+        }
+    }
+
+    private fun cardImageResource(card: Card?): Int = resources.getIdentifier(fileName(card), "drawable", requireContext().packageName)
+
+    private fun setCardViewsTags() {
+        var i = 0
+        binding.cardView1.tag = i++
+        binding.cardView2.tag = i++
+        binding.cardView3.tag = i++
+        binding.cardView4.tag = i++
+        binding.cardView5.tag = i++
+        binding.cardView6.tag = i++
+        binding.cardView7.tag = i++
+        binding.cardView8.tag = i++
+        binding.cardView9.tag = i++
+        binding.cardView10.tag = i++
+        binding.cardView11.tag = i++
+        binding.cardView12.tag = i++
+        binding.cardView13.tag = i++
+        binding.cardView14.tag = i++
+        binding.cardView15.tag = i++
+        binding.cardView16.tag = i++
+        binding.cardView17.tag = i++
+        binding.cardView18.tag = i++
+        binding.cardView19.tag = i++
+        binding.cardView20.tag = i++
+        binding.cardView21.tag = i++
+        binding.cardView22.tag = i++
+        binding.cardView23.tag = i++
+        binding.cardView24.tag = i++
+        binding.cardView25.tag = i++
+        binding.cardView26.tag = i++
+        binding.cardView27.tag = i++
+        binding.cardView28.tag = i++
+        binding.cardView29.tag = i++
+        binding.cardView30.tag = i
     }
 
     override fun onDestroyView() {
