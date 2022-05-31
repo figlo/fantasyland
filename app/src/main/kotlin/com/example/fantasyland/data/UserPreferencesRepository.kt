6f68fb83c9805aea646fe306b29d0c9c7ea65ea2
figlo <miro@figlar.sk>
2022-05-31 20:52:17 +0200

    added userPreferencesFlow, mapUserPreferences, updateNumberOfCardsInFantasyLand

--- app/src/main/kotlin/com/example/fantasyland/data/UserPreferencesRepository.kt
+++ app/src/main/kotlin/com/example/fantasyland/data/UserPreferencesRepository.kt
@@ -3,8 +3,15 @@ package com.example.fantasyland.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.core.intPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import timber.log.Timber
+import java.io.IOException
 
 private const val USER_PREFERENCES_NAME = "user_preferences"
 
@@ -12,13 +19,37 @@ private val Context.dataStore by preferencesDataStore(
     name = USER_PREFERENCES_NAME
 )
 
-data class UserPreferences (
-    val numberOfCards: Int
+data class UserPreferences(
+    val numberOfCardsInFantasyLand: Int
 )
 
 class UserPreferencesRepository(private val dataStore: DataStore<Preferences>) {
 
     private object PreferencesKeys {
         val NUMBER_OF_CARDS_IN_FANTASY_LAND = intPreferencesKey("number_of_cards")
+    }
+
+    val userPreferencesFlow: Flow<UserPreferences> = dataStore.data
+        .catch { exception ->
+            if (exception is IOException) {
+                Timber.e("Error reading preferences")
+                emit(emptyPreferences())
+            } else {
+                throw(exception)
+            }
+        }.map { preferences ->
+            mapUserPreferences(preferences)
+        }
+
+    private fun mapUserPreferences(preferences: Preferences): UserPreferences {
+        val numberOfCardsInFantasyLand = preferences[PreferencesKeys.NUMBER_OF_CARDS_IN_FANTASY_LAND] ?: 14
+        return UserPreferences(numberOfCardsInFantasyLand)
+
+    }
+
+    suspend fun updateNumberOfCardsInFantasyLand(numberOfCardsInFantasyLand: Int) {
+        dataStore.edit { preferences ->
+            preferences[PreferencesKeys.NUMBER_OF_CARDS_IN_FANTASY_LAND] = numberOfCardsInFantasyLand
+        }
     }
 }
