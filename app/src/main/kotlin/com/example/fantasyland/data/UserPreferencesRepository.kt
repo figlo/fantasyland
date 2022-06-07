ed30f55dfc775659e859fdda4db366429ce4f4a8
figlo <miro@figlar.sk>
2022-06-07 22:07:55 +0200

    refactoring

--- app/src/main/kotlin/com/example/fantasyland/data/UserPreferencesRepository.kt
+++ app/src/main/kotlin/com/example/fantasyland/data/UserPreferencesRepository.kt
@@ -1,25 +1,16 @@
 package com.example.fantasyland.data
 
-import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
-import androidx.datastore.preferences.core.*
-import androidx.datastore.preferences.preferencesDataStoreFile
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.intPreferencesKey
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import timber.log.Timber
 import java.io.IOException
 import javax.inject.Inject
-import javax.inject.Singleton
 
 data class UserPreferences(
     val numberOfCardsInFantasyLand: Int
@@ -53,24 +44,5 @@ class UserPreferencesRepository @Inject constructor(private val dataStore: DataS
         dataStore.edit { preferences ->
             preferences[PreferencesKeys.NUMBER_OF_CARDS_IN_FANTASY_LAND] = numberOfCardsInFantasyLand
         }
-    }
-}
-
-private const val USER_PREFERENCES = "user_preferences"
-
-@InstallIn(SingletonComponent::class)
-@Module
-object DataStoreModule {
-
-    @Singleton
-    @Provides
-    fun providePreferencesDataStore(@ApplicationContext appContext: Context): DataStore<Preferences> {
-        return PreferenceDataStoreFactory.create(
-            corruptionHandler = ReplaceFileCorruptionHandler(
-                produceNewData = { emptyPreferences() }
-            ),
-            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
-            produceFile = { appContext.preferencesDataStoreFile(USER_PREFERENCES) }
-        )
     }
 }
