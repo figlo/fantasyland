15a432a2e9cef0a171095bd051629911d3b765e6
figlo <miro@figlar.sk>
2022-06-18 23:18:36 +0200

    initial commit

---
+++ app/src/androidTest/java/com/example/fantasyland/FantasyLandDaoTest.kt
@@ -0,0 +1,45 @@
+package com.example.fantasyland
+
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.fantasyland.data.FantasyLandDao
+import com.example.fantasyland.data.FantasyLandDatabase
+import com.example.fantasyland.data.Game
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class FantasyLandDaoTest {
+
+    private lateinit var db: FantasyLandDatabase
+    private lateinit var dao: FantasyLandDao
+
+    @Before
+    fun createDb() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+        db = Room.inMemoryDatabaseBuilder(context, FantasyLandDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+        dao = db.fantasyLandDao
+    }
+
+    @After
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    fun insertAndGetGame() {
+        val game = Game()
+        dao.insert(game)
+        assertEquals(1, dao.count())
+        dao.clear()
+        assertEquals(0, dao.count())
+    }
+}
