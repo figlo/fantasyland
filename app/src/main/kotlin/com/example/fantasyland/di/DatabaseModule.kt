0b510220727fef6cc460e3692ac228e62b7cf614
figlo <miro@figlar.sk>
2022-06-20 16:33:21 +0200

    initial commit

---
+++ app/src/main/kotlin/com/example/fantasyland/di/DatabaseModule.kt
@@ -0,0 +1,25 @@
+package com.example.fantasyland.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.fantasyland.data.FantasyLandDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideFantasyLandDatabase(@ApplicationContext appContext: Context) =
+        Room.databaseBuilder(
+            appContext,
+            FantasyLandDatabase::class.java,
+            "fantasy_land_database"
+        ).build()
+}
