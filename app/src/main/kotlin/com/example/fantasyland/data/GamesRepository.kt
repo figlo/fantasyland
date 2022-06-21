f45cffb8a924b5824002e5c48ffe836f249681e2
figlo <miro@figlar.sk>
2022-06-21 21:11:40 +0200

    deleted GamesRepository

--- app/src/main/kotlin/com/example/fantasyland/data/GamesRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.fantasyland.data
-
-import javax.inject.Inject
-
-class GamesRepository @Inject constructor(
-    private val fantasyLandDao: FantasyLandDao
-){
-    fun numberOfItemsInDB() = fantasyLandDao.count()
-}
