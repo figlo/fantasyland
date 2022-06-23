3697489545b046de4e696a1c41f75279d5f0d81a
figlo <miro@figlar.sk>
2022-06-23 21:32:43 +0200

    localized datetime

--- app/src/main/kotlin/com/example/fantasyland/GamesArchiveViewModel.kt
+++ app/src/main/kotlin/com/example/fantasyland/GamesArchiveViewModel.kt
@@ -1,49 +1,32 @@
 package com.example.fantasyland
 
-import android.app.Application
-import android.content.res.Resources
 import android.text.Spanned
 import androidx.core.text.HtmlCompat
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.ViewModel
 import com.example.fantasyland.data.FantasyLandDao
 import com.example.fantasyland.data.Game
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 import javax.inject.Inject
 
 @HiltViewModel
 class GamesArchiveViewModel @Inject constructor(
-    private val dao: FantasyLandDao,
-    application: Application
+    dao: FantasyLandDao
 ) : ViewModel() {
 
     private val games: LiveData<List<Game>> = dao.getAllGames()
     val gamesString = Transformations.map(games) { games ->
-        formatGames(games, application.resources)
-    }
-
-    var count: MutableLiveData<Long> = MutableLiveData(0)
-
-    init {
-        getCount()
-    }
-
-    private fun getCount() {
-        viewModelScope.launch {
-            count.value = suspendCount()
-        }
-    }
-
-    private suspend fun suspendCount(): Long {
-        return dao.count()
+        formatGames(games)
     }
 }
 
-fun formatGames(games: List<Game>, resources: Resources): Spanned {
+fun formatGames(games: List<Game>): Spanned {
     val sb = StringBuilder()
-    val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy hh:mm:ss").withZone(ZoneId.systemDefault())
+    val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).withZone(ZoneId.systemDefault())
     sb.apply {
         append("Games: ")
         append(games.size)
