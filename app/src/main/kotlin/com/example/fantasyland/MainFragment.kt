678baf96df80ae3a2b9bda5c161720a1c1b2888d
figlo <miro@figlar.sk>
2022-07-07 22:08:27 +0200

    refactoring

--- app/src/main/kotlin/com/example/fantasyland/MainFragment.kt
+++ app/src/main/kotlin/com/example/fantasyland/MainFragment.kt
@@ -5,15 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.fantasyland.databinding.FragmentMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainFragment : Fragment() {
-
-    private val viewModel: MainViewModel by viewModels()
 
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
@@ -28,28 +25,28 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        fun buttonListener(numberOfCards: Int) {
-            viewModel.setNumberOfCardsInFantasyLand(numberOfCards)
+        fun navigateToGameFragment(numberOfCards: Int) {
             this.findNavController().navigate(MainFragmentDirections.actionMainFragmentToGameFragment(numberOfCards))
         }
+
         binding.button13.setOnClickListener {
-            buttonListener(13)
+            navigateToGameFragment(13)
         }
 
         binding.button14.setOnClickListener {
-            buttonListener(14)
+            navigateToGameFragment(14)
         }
 
         binding.button15.setOnClickListener {
-            buttonListener(15)
+            navigateToGameFragment(15)
         }
 
         binding.button16.setOnClickListener {
-            buttonListener(16)
+            navigateToGameFragment(16)
         }
 
         binding.button17.setOnClickListener {
-            buttonListener(17)
+            navigateToGameFragment(17)
         }
     }
 
