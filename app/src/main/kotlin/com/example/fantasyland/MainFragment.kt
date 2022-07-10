a261091d44c809391902b75b62c5623b3bb9cda9
figlo <miro@figlar.sk>
2022-07-10 23:30:56 +0200

    set buttons dimensions

--- app/src/main/kotlin/com/example/fantasyland/MainFragment.kt
+++ app/src/main/kotlin/com/example/fantasyland/MainFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.view.setMargins
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.window.layout.WindowMetricsCalculator
 import com.example.fantasyland.databinding.FragmentMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,7 +18,7 @@ class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var buttonViews: List<Button>
+    private lateinit var buttons: List<Button>
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,7 +30,8 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        setUpButtonViews()
+        setUpButtonsList()
+        setUpButtonsParams()
         setUpNavigationToGameFragment()
     }
 
@@ -37,9 +40,9 @@ class MainFragment : Fragment() {
         _binding = null
     }
 
-    private fun setUpButtonViews() {
+    private fun setUpButtonsList() {
         binding.apply {
-            buttonViews = listOf(
+            buttons = listOf(
                 button13,
                 button14,
                 button15,
@@ -49,12 +52,38 @@ class MainFragment : Fragment() {
         }
     }
 
+    private fun setUpButtonsParams() {
+        // calculating button width and height
+        val bounds = WindowMetricsCalculator
+            .getOrCreate()
+            .computeCurrentWindowMetrics(requireActivity())
+            .bounds
+        val displayWidth = bounds.width()
+        val buttonWidth: Int = displayWidth / 6                  // empirically found out
+        val buttonHeight: Int = buttonWidth
+
+        // margin must be set again programmatically (allready set in xml)
+        val buttonMargin = 20
+
+        for (button in buttons) {
+            with(button) {
+                // setup button width and height
+                layoutParams = android.widget.LinearLayout.LayoutParams(buttonWidth, buttonHeight)
+
+                // setup button margins
+                val params = button.layoutParams as ViewGroup.MarginLayoutParams
+                params.setMargins(buttonMargin)
+                layoutParams = params
+            }
+        }
+    }
+
     private fun setUpNavigationToGameFragment() {
         fun navigateToGameFragment(numberOfCards: Int) {
             this.findNavController().navigate(MainFragmentDirections.actionMainFragmentToGameFragment(numberOfCards))
         }
 
-        for ((index, button) in buttonViews.withIndex()) {
+        for ((index, button) in buttons.withIndex()) {
             button.setOnClickListener {
                 navigateToGameFragment(index + 13)
             }
