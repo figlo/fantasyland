6b45b49f36b8225b636827965c9347f9b432d108
figlo <miro@figlar.sk>
2022-07-10 22:33:41 +0200

    refactoring

--- app/src/main/kotlin/com/example/fantasyland/MainFragment.kt
+++ app/src/main/kotlin/com/example/fantasyland/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.fantasyland.databinding.FragmentMainBinding
@@ -15,6 +16,8 @@ class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var buttonViews: List<Button>
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -25,33 +28,36 @@ class MainFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        fun navigateToGameFragment(numberOfCards: Int) {
-            this.findNavController().navigate(MainFragmentDirections.actionMainFragmentToGameFragment(numberOfCards))
-        }
-
-        binding.button13.setOnClickListener {
-            navigateToGameFragment(13)
-        }
-
-        binding.button14.setOnClickListener {
-            navigateToGameFragment(14)
-        }
-
-        binding.button15.setOnClickListener {
-            navigateToGameFragment(15)
-        }
-
-        binding.button16.setOnClickListener {
-            navigateToGameFragment(16)
-        }
-
-        binding.button17.setOnClickListener {
-            navigateToGameFragment(17)
-        }
+        setUpButtonViews()
+        setUpNavigationToGameFragment()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun setUpButtonViews() {
+        binding.apply {
+            buttonViews = listOf(
+                button13,
+                button14,
+                button15,
+                button16,
+                button17,
+            )
+        }
+    }
+
+    private fun setUpNavigationToGameFragment() {
+        fun navigateToGameFragment(numberOfCards: Int) {
+            this.findNavController().navigate(MainFragmentDirections.actionMainFragmentToGameFragment(numberOfCards))
+        }
+
+        for ((index, button) in buttonViews.withIndex()) {
+            button.setOnClickListener {
+                navigateToGameFragment(index + 13)
+            }
+        }
     }
 }
